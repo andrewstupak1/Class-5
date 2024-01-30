1f181a1005d34b343475fe90c596dd9f2f4ca2fb
Andrew Stupak <andrew.stupak@kellogg.northwestern.edu>
2024-01-30 17:18:29 +0000

    update 2

--- labs/2-models.rb
+++ labs/2-models.rb
@@ -17,19 +17,51 @@ Salesperson.destroy_all
 #   full name of each salesperson.
 
 # 1a. check out the schema file
+
+
 # 1b. check out the model file
 
 # 2. insert 1-2 rows in salespeople table.
 
+new_salesperson = Salesperson.new
+new_salesperson["first_name"] = "Andrew"
+new_salesperson["last_name"] = "Stupak"
+new_salesperson["email"] = "andrew.stupak@gmail.com"
+new_salesperson.save
+
+new_salesperson = Salesperson.new
+new_salesperson["first_name"] = "John"
+new_salesperson["last_name"] = "Smith"
+new_salesperson["email"] = "John.Smith@gmail.com"
+new_salesperson.save
+
+
 # 3. write code to display how many salespeople rows are in the database
 
-# ---------------------------------
+puts "salespople: #{Salesperson.all.count}"
+
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
+#find John Smith 
+John = Salesperson.find_by({"first_name" => "John", "last_name" => "Smith"})
+# assign email column 
+John["email"] = "john@gmail.com"
+# save
+John.save
 
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
+Salesperson.all.each do |salesperson|
+    puts "#{salesperson['first_name']} #{salesperson['last_name']}"
+  end
+
+  #OR 
+  #person can be anything 
+  salespeople = Salesperson.all
+  for person in salespeople
+    puts "#{person['first_name']} #{person['last_name']}"
+  end
 
 # ---------------------------------
 # Salespeople: 2
